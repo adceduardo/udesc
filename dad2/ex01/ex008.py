1b0a20f64f594a4c40d00103fe2155e54ba2efe5
EDUARDO ALVES DE CARVALHO <105119276+adceduardo@users.noreply.github.com>
2026-03-10 19:47:02 -0300

    [refactor] melhoria nas funcionalidades do exercicio 008

--- dad2/ex01/ex008.py
+++ dad2/ex01/ex008.py
@@ -5,40 +5,58 @@ def saveContact(name, phone):
     print("\nContato salvo com sucesso!")
 
 def listContacts():
-    with open("contatos.txt", "r") as archive:
-        print(archive.readlines())
+    try:
+        with open("contatos.txt", "r") as archive:
+            print(f"\n{'Nome':<15}{'Telefone'}")
+            print("-" * 28)
+            
+            for line in archive:
+                contact = line.strip().split(",")
+                print(f"{contact[0]:<15}{contact[1]}")
+    except:
+        print("\nAdicione um contato antes de consultar a lista")
 
 def searchContact(name):
-    list = []
+    try:
+        list = []
+        with open("contatos.txt", "r") as arquive:
+            for line in arquive:
+                if name.lower() in line.lower():
+                    print(line)
+                    list.append(line.split(",")) 
 
-    with open("contatos.txt", "r") as arquive:
-        for line in arquive:
-            if name in line:
-                list.append(line)
-
-        if len(list) < 1:
-            print("\nNenhum contato encontrado")
-        
-        else:
-            print(list)
+            if len(list) < 1:
+                print("\nNenhum contato encontrado")
+            
+            else:
+                print(f"\n{'Nome':<15}{'Telefone'}")
+                print("-" * 28)
+                
+                for contact in list:
+                    print(f"{contact[0]:<15}{contact[1]}")
+    except:
+         print("\nAdicione um contato antes de consultar a lista")
 
 while True:
     print("\nMenu: ")
     print("1. Adicionar Contato\n2. Listar contatos \n3. Buscar contato \n4. Sair")
     
-    opc = int(input("\nDigite uma opção: "))
+    opc = input("\nDigite uma opção: ")
     
-    if opc == 1:
+    if opc == "1":
         name = input("\nDigite o nome do contato: ")
         phone = input("\nDigite o telefone: ")
         saveContact(name, phone)
 
-    elif opc == 2:
+    elif opc == "2":
         listContacts()
 
-    elif opc == 3:
+    elif opc == "3":
         name = input("\nDigite o nome que deseja encontrar: ")
         searchContact(name)
 
-    elif opc == 4:
+    elif opc == "4":
         break
+    
+    else:
+        print("\nDigite uma opção válida")
