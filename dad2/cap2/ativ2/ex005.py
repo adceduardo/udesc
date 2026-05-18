0bb31de2478cde9b88fc8d71136d5ceb37473bed
EDUARDO ALVES DE CARVALHO <105119276+adceduardo@users.noreply.github.com>
2026-05-18 19:30:19 -0300

    [chore] complemento retroativo

--- dad2/cap2/ativ2/ex005.py
+++ dad2/cap2/ativ2/ex005.py
@@ -3,14 +3,18 @@ def compress(uncompressed):
     dict_size = 256
     dictionary = {chr(i): i for i in range(dict_size)}
 
-    print(dictionary)
-
     w = ""
     result = []
     for c in uncompressed:
-        # COMPLETE
-        return
- 
+        wc = w + c
+        if wc in dictionary:
+            w = wc
+        else:
+            result.append(dictionary[w])
+            dictionary[wc] = dict_size
+            dict_size += 1
+            w = c
+    
     if w:
         result.append(dictionary[w])
     return result
