d00fcfa31a9f73d362fd64df4bffeb9e0dc7307e
EDUARDO ALVES DE CARVALHO <105119276+adceduardo@users.noreply.github.com>
2026-05-31 22:37:10 -0300

    [feat] revision cap3

--- dad2/cap3/ativ3/revisão .py
+++ dad2/cap3/ativ3/revisão .py
@@ -1,55 +1,130 @@
-
 def bubble(arr):
     n = len(arr)
 
-    for i in range(0, n-1):
-        for j in range(0, n-1):
-            if(arr[j] > arr[j+1]):
-                aux = arr[j+1]
-                arr[j+1] = arr[j]
-                arr[j] = aux
+    print(arr)
 
-    return(arr)
+    for i in range(n-1):
+
+        for j in range(n-1):
+            if arr[j] > arr[j+1]:
+                aux = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = aux
+    print(arr)
 
 def quick(arr):
     n = len(arr)
 
-    if n <= 1:
+    if n <=1:
         return arr
+    
+    left, right, middle = [], [], []
+    middle = n//2
+    pivot = arr[middle]
 
-    pivot = n//2
-    pivot = arr[pivot]
 
-    left, right = [], []
-
-    for i in arr:
-
-        if i == pivot:
+    for i in range(n):
+        if arr[i] == pivot:
+            middle.append(arr[i])
             continue
 
-        if i < pivot:
-            left.append(i)
-
+        if arr[i] < pivot:
+            left.append(arr[i])
         else:
-            right.append(i)
+            right.append(arr[i])
 
-    return(quick(left) + [pivot] +  quick(right))
+    
+    return quick(left) + middle + quick(right)
 
-def insertion(arr):
+def selection(arr):
+    print(arr)
     n = len(arr)
 
-    for i in range(1, n):        
-        aux = arr[i]
-        
-        j = i-1
-        while j >=0 and aux < arr[j]:
-            arr[j+1] = arr[j]
+    for i in range(n):
+        minor = i
 
+        for j in range(i+1, n):
+            if(arr[j] < arr[minor]):
+                minor = j
+
+        aux = arr[i]
+        arr[i] = arr[minor]
+        arr[minor] = aux
+
+    print(arr)
+
+def insertion(arr):
+    print(arr)
+    n = len(arr)
+
+    for i in range(1, n):
+        j = i-1
+        aux = arr[i]
+
+        while j >=0 and arr[j] > aux:
+            arr[j+1] = arr[j]
             j-=1
 
-        arr[j+1] = aux
+        arr[j+1] = aux   
     
-    return(arr)
+    print(arr)      
+            
+def merge(arr):
+    n = len(arr)
 
-arr = [10, 4, 7, 2, 9]
+    if n <= 1:
+        return arr
+    
+    middle = n//2
 
+    left = arr[:middle]
+    rigth = arr[middle:]
+
+    newL = merge(left)
+    newR = merge(rigth)
+
+    output = []
+
+    topL, topR = 0, 0
+
+    while topL < len(newL) and topR < len(newR):
+        if newR[topR] < newL[topL]:
+            output.append(newR[topR])
+            topR += 1
+
+        else:
+            output.append(newL[topL])
+            topL +=1
+
+    output += newL[topL:] 
+    output += newR[topR:]
+   
+    return output
+
+def shell(arr):
+    print(arr)
+    n = len(arr)
+    gap = n//2
+
+    while gap > 0:
+        print('gap', gap)
+
+        for i in range(gap, n):
+            aux = arr[i]
+            j = i - gap
+
+            print(aux, arr[j])
+
+
+
+        gap = gap//2
+
+    
+
+
+    
+
+
+arr = [9, 8, 3, 7, 5, 6, 4, 1]
+
+shell(arr)
